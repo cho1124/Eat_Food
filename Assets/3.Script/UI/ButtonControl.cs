24cd9665b00c6811b74156329852dd85d0e53e99
zzangmin <wjdals1212gh@gmail.com>
2024-06-28 11:18:16 +0900

    [Update] SelectChar Scene
    캐릭터 변경 잘 됩니다

--- Assets/3.Script/UI/ButtonControl.cs
+++ Assets/3.Script/UI/ButtonControl.cs
@@ -1,41 +1,142 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Linq;
+using TMPro;
 
 public class ButtonControl : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject[] player;
+    public Scene currentScene;
+    public TextMeshProUGUI info;
 
-    public GameObject[] Animals;
+    private void Awake()
+    {
+        currentScene = SceneManager.GetActiveScene();
+        if (currentScene.name == "SelectChar")
+        {
+            player = new GameObject[3];
 
-    
+            // 비활성화된 오브젝트 포함하여 모든 오브젝트 찾기
+            var allObjects = Resources.FindObjectsOfTypeAll<GameObject>().Where(obj => obj.hideFlags == HideFlags.None).ToArray();
+            info = GameObject.FindObjectOfType<TextMeshProUGUI>();
+
+            for (int i = 1; i < 4; i++)
+            {
+                player[i - 1] = allObjects.FirstOrDefault(obj => obj.name == "Player" + i.ToString());
+                if (player[i - 1] == null)
+                {
+                    Debug.LogError("Player" + i.ToString() + " not found!");
+                }
+            }
+        }
+    }
+
     public void PlayButton()
     {
         SceneManager.LoadScene("SelectChar");
     }
+
     public void ExitButton()
     {
-        #if UNITY_EDITOR
+#if UNITY_EDITOR
         UnityEditor.EditorApplication.isPlaying = false;
-        #else
+#else
         Application.Quit();
-        #endif
+#endif
     }
 
-    
-    
 
-    void LeftButton()
+
+    //-----------------------------------------SceneChar--------------------------------------//
+
+    public void ShowCharInfo()
     {
+        if(GameManager.instance.selectedCharacter==0)
+        {
+            info.text = "플레이어 1\n\n플레이어1입니다\n설명을 추가해주세요";
 
+        }
+        else if (GameManager.instance.selectedCharacter == 1)
+        {
+            info.text = "플레이어 2\n\n플레이어2입니다\n설명을 추가해주세요";
+        }
+        else
+        {
+            info.text = "플레이어 3\n\n플레이어3입니다\n설명을 추가해주세요";
+        }
     }
 
-    void RightButton()
+
+
+
+    public void LeftButton()
     {
+        if (GameManager.instance == null)
+        {
+            Debug.LogError("GameManager instance is not set!");
+            return;
+        }
 
+        Debug.Log("Current selected character: " + GameManager.instance.selectedCharacter);
+
+        if (GameManager.instance.selectedCharacter == 0)
+        {
+            player[GameManager.instance.selectedCharacter]?.SetActive(false);
+            GameManager.instance.selectedCharacter = 2;
+            player[GameManager.instance.selectedCharacter]?.SetActive(true);
+            ShowCharInfo();
+        }
+        else
+        {
+            player[GameManager.instance.selectedCharacter]?.SetActive(false);
+            GameManager.instance.selectedCharacter--;
+            player[GameManager.instance.selectedCharacter]?.SetActive(true);
+            ShowCharInfo();
+        }
     }
 
-    void ConfirmButton()
+
+
+
+
+
+    public void RightButton()
     {
+        if (GameManager.instance == null)
+        {
+            Debug.LogError("GameManager instance is not set!");
+            return;
+        }
 
+        Debug.Log("Current selected character: " + GameManager.instance.selectedCharacter);
+
+        if (GameManager.instance.selectedCharacter == 2)
+        {
+            player[GameManager.instance.selectedCharacter]?.SetActive(false);
+            GameManager.instance.selectedCharacter = 0;
+            player[GameManager.instance.selectedCharacter]?.SetActive(true);
+            ShowCharInfo();
+        }
+        else
+        {
+            player[GameManager.instance.selectedCharacter]?.SetActive(false);
+            GameManager.instance.selectedCharacter++;
+            player[GameManager.instance.selectedCharacter]?.SetActive(true);
+            ShowCharInfo();
+        }
     }
+
+    public void ConfirmButton()
+    {
+        if (GameManager.instance == null)
+        {
+            Debug.LogError("GameManager instance is not set!");
+            return;
+        }
+
+        Debug.Log("Confirming character: " + GameManager.instance.selectedCharacter);
+        SceneManager.LoadScene("SampleScene");
+    }
+    //-----------------------------------------------------------------------------------------//
+
 }
