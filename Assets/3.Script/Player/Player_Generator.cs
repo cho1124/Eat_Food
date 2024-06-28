b8204ea5927934c651f4404d0ddcd1bae37386fb
cho1124 <122534567+cho1124@users.noreply.github.com>
2024-06-28 11:21:34 +0900

    [Update] Character Controller doing
    doing

--- Assets/3.Script/Player/Player_Generator.cs
+++ Assets/3.Script/Player/Player_Generator.cs
@@ -22,7 +22,8 @@ public class Player_Generator : MonoBehaviour
         player = Instantiate(player_data.character_prefab);
         playerControl = player.AddComponent<PlayerControl>();
         playerControl.speed = player_data.moveSpeed;
-
+        playerControl.characterType = player_data.CharacterType;
+        
 
     }
 
