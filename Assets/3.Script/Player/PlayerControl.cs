3403fca207227c8ae703b9f505bc3c2cb2318e76
cho1124 <122534567+cho1124@users.noreply.github.com>
2024-06-28 11:21:34 +0900

    [Update] Character Controller doing
    doing

--- Assets/3.Script/Player/PlayerControl.cs
+++ Assets/3.Script/Player/PlayerControl.cs
@@ -6,11 +6,12 @@ public class PlayerControl : MonoBehaviour
 {
 
     public float speed = 10f;
-    public Player_Data Player_Data;
+    public CharacterController playerCTRL;
+    public CharacterType characterType;
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerCTRL = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
@@ -24,6 +25,32 @@ public class PlayerControl : MonoBehaviour
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
         Vector3 movement = new Vector3(x, 0, z) * speed * Time.deltaTime;
-        transform.Translate(movement, Space.Self);
+        playerCTRL.Move(movement);
+        //transform.Translate(movement, Space.Self);
+        characterSkill();
     }
+
+    private void characterSkill()
+    {
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            switch (characterType)
+            {
+                case CharacterType.a:
+                    Debug.Log("a skilled");
+                    break;
+                case CharacterType.b:
+                    Debug.Log("b skilled");
+                    break;
+                case CharacterType.c:
+                    Debug.Log("c skilled");
+                    break;
+
+            }
+        }
+    }
+
+        
+
 }
